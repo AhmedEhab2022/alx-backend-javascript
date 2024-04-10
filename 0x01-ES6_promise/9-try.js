c8d9e7485c14555dd62328b7a8d6be6a53ac96ab
AhmedEhab2022 <am8122004@gmail.com>
2024-04-10 16:23:47 +0200

    9-try.js

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,7 +1,7 @@
 export default function guardrail(mathFunction) {
   try {
-    return [mathFunction(), "Guardrail was processed"];
+    return [mathFunction(), 'Guardrail was processed'];
   } catch (e) {
-    return ["Error: " + e.message, "Guardrail was processed"];
+    return ['Error: ' + e.message, 'Guardrail was processed'];
   }
 }
