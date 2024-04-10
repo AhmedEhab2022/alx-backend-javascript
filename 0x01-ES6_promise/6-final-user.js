f602e9b94147b31ce00b34d7c1cb24bbdde02639
AhmedEhab2022 <am8122004@gmail.com>
2024-04-10 14:56:06 +0200

    6-final-user.js

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,12 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default function handleProfileSignup(firstName, lastName, filename) {
+  return Promise.all([uploadPhoto(filename), signUpUser(firstName, lastName)])
+    .then((values) => {
+      console.log(
+        `${values[0].message} ${values[1].firstName} ${values[1].lastName}`
+      );
+    })
+    .catch((error) => console.log(error.message));
+}
