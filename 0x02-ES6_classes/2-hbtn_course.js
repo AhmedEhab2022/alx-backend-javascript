36fe32ea14c6e420f012a94915b4586ca4109b64
AhmedEhab2022 <am8122004@gmail.com>
2024-04-11 18:29:13 +0200

    fix style

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -7,6 +7,7 @@ export default class HolbertonCourse {
     } else if (!Array.isArray(students)) {
       throw TypeError('Students must be an array');
     }
+
     this._name = name;
     this._length = length;
     this._students = students;
@@ -14,24 +15,29 @@ export default class HolbertonCourse {
   get name() {
     return this._name;
   }
+
   set name(value) {
     if (typeof value !== 'string') {
       throw TypeError('Name must be a string');
     }
     this._name = value;
   }
+
   get length() {
     return this._length;
   }
+
   set length(value) {
     if (typeof value !== 'number') {
       throw TypeError('Length must be a number');
     }
     this._length = value;
   }
+
   get students() {
     return this._students;
   }
+
   set students(value) {
     if (!Array.isArray(value)) {
       throw TypeError('Students must be an array');
