dcddd41146a7080c2be6f60db1a11793e531cbd0
AhmedEhab2022 <am8122004@gmail.com>
2024-04-11 18:02:02 +0200

    8-hbtn_class.js

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,17 @@
+export default class HolbertonClass {
+  constructor(size, location, teacher) {
+    this._size = size;
+    this._location = location;
+    this._teacher = teacher;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    }
+    if (hint === 'string') {
+      return this._location;
+    }
+    return null;
+  }
+}
