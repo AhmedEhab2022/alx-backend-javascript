3e33d89fa10b8148a1526274919587423aacfb79
AhmedEhab2022 <am8122004@gmail.com>
2024-04-11 17:02:39 +0200

    1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -3,6 +3,6 @@ import ClassRoom from './0-classroom';
 export default function initializeRooms() {
   const classRoom1 = new ClassRoom(19);
   const classRoom2 = new ClassRoom(20);
-  const classRoom3 = new ClassRoom(15);
+  const classRoom3 = new ClassRoom(34);
   return [classRoom1, classRoom2, classRoom3];
 }
