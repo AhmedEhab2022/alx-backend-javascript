6928f2df0fe46e057f26b8fa18f203a659054436
AhmedEhab2022 <am8122004@gmail.com>
2024-04-21 20:47:37 +0200

    task_5/

---
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,19 @@
+interface MajorCredits {
+  credits: number;
+  brand: string;
+}
+
+interface MinorCredits {
+  credits: number;
+  brand: string;
+}
+
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+  const sum = subject1.credits + subject2.credits;
+  return { credits: sum, brand: 'Major' };
+}
+
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+  const sum = subject1.credits + subject2.credits;
+  return { credits: sum, brand: 'Minor' };
+}
