ed2185669802d0d2454421f8cf6fa6bfbab9b0a8
AhmedEhab2022 <am8122004@gmail.com>
2024-06-18 20:57:12 +0300

    3. Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,34 +1,34 @@
 const fs = require('fs');
 
-async function countStudents(databasePath) {
-  try {
-    const data = await fs.promises.readFile(databasePath, 'utf8');
-    let numOfCsStudents = 0;
-    let numOfSweStudents = 0;
-    const listOfCsFirstnames = [];
-    const listOfSweFirstnames = [];
-    const students = data.split('\r\n').slice(1);
-    for (let i = students.length - 1; i >= 0; i -= 1) {
-      if (students[i] === '') {
-        students.splice(i, 1);
+function countStudents(databasePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(databasePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
       }
-    }
-    console.log(`Number of students: ${students.length}`);
-    students.forEach((student) => {
-      const studentArray = student.split(',');
-      if (studentArray[3] === 'CS') {
-        listOfCsFirstnames.push(studentArray[0]);
-        numOfCsStudents += 1;
-      } else if (studentArray[3] === 'SWE') {
-        listOfSweFirstnames.push(studentArray[0]);
-        numOfSweStudents += 1;
+      const students = data.split('\r\n').slice(1);
+      for (let i = students.length - 1; i >= 0; i -= 1) {
+        if (students[i] === '') {
+          students.splice(i, 1);
+        }
       }
+      console.log(`Number of students: ${students.length}`);
+      const listOfCsFirstnames = [];
+      const listOfSweFirstnames = [];
+      students.forEach((student) => {
+        const studentArray = student.split(',');
+        if (studentArray[3] === 'CS') {
+          listOfCsFirstnames.push(studentArray[0]);
+        } else if (studentArray[3] === 'SWE') {
+          listOfSweFirstnames.push(studentArray[0]);
+        }
+      });
+      console.log(`Number of students in CS: ${listOfCsFirstnames.length}. List: ${listOfCsFirstnames.join(', ')}`);
+      console.log(`Number of students in SWE: ${listOfSweFirstnames.length}. List: ${listOfSweFirstnames.join(', ')}`);
+      resolve();
     });
-    console.log(`Number of students in CS: ${numOfCsStudents}. List: ${listOfCsFirstnames.join(', ')}`);
-    console.log(`Number of students in SWE: ${numOfSweStudents}. List: ${listOfSweFirstnames.join(', ')}`);
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
+  });
 }
 
 module.exports = countStudents;
