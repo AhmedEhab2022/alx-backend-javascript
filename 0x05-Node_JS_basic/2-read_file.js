2f44d5a38ae4039c65a493d14a7a0762acfcf1b1
AhmedEhab2022 <am8122004@gmail.com>
2024-06-18 17:40:55 +0300

    2. Reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+function countStudents(databasePath) {
+  if (fs.existsSync(databasePath)) {
+    const data = fs.readFileSync(databasePath, 'utf8');
+    let numOfCsStudents = 0;
+    let numOfSweStudents = 0;
+    const listOfCsFirstnames = [];
+    const listOfSweFirstnames = [];
+    const students = data.split('\r\n').slice(1);
+    console.log(`Number of students: ${students.length}`);
+    students.forEach((student) => {
+      const studentArray = student.split(',');
+      if (studentArray[3] === 'CS') {
+        listOfCsFirstnames.push(studentArray[0]);
+        numOfCsStudents += 1;
+      } else {
+        listOfSweFirstnames.push(studentArray[0]);
+        numOfSweStudents += 1;
+      }
+    });
+    console.log(`Number of students in CS: ${numOfCsStudents}. List: ${listOfCsFirstnames.join(', ')}`);
+    console.log(`Number of students in SWE: ${numOfSweStudents}. List: ${listOfSweFirstnames.join(', ')}`);
+  } else {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
