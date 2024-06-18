2adb9eb4ef07a2b5d3552b9fe5c4aaa6b27974c8
AhmedEhab2022 <am8122004@gmail.com>
2024-06-18 21:20:42 +0300

    5. Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -7,19 +7,21 @@ const app = http.createServer((req, res) => {
     res.write('Hello Holberton School!');
     res.end();
   } else if (req.url === '/students') {
-    countStudents('database.csv')
+    res.write('This is the list of our students\n');
+    countStudents(process.argv[2])
       .then((data) => {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.write('This is the list of our students\n');
-        res.write(data.join('\n'));
+        res.write(`Number of students: ${data[0]}\n`);
+        res.write(`Number of students in CS: ${data[1].length}. List: ${data[1].join(', ')}\n`);
+        res.write(`Number of students in SWE: ${data[2].length}. List: ${data[2].join(', ')}\n`);
         res.end();
       })
       .catch((error) => {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.write(error.message);
         res.end();
       });
   }
 });
+
+app.listen(1245);
 
 module.exports = app;
