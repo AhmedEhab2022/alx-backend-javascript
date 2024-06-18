19799c6e14088763e1c4c9b48bd704f3fe7abdbf
AhmedEhab2022 <am8122004@gmail.com>
2024-06-18 15:07:46 +0300

    1. Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+process.stdin.setEncoding('utf8');
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}\n`);
+  }
+});
+
+process.on('exit', () => {
+  if (process.stdin.isTTY) {
+    process.stdout.write('This important software is now closing\n');
+  }
+});
