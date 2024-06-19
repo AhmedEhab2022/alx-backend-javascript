adfe75bce11ea869f795921c6894b44d46dc2470
AhmedEhab2022 <am8122004@gmail.com>
2024-06-19 17:47:01 +0300

    4. Stubs

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,14 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should log the total', () => {
+    const consoleSpy = sinon.spy(console, 'log');
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleSpy.calledOnceWithExactly('The total is: 10')).to.be.true;
+    expect(calculateNumberStub.calledOnceWithExactly(100, 20, 'SUM')).to.be.true;
+    consoleSpy.restore();
+    calculateNumberStub.restore();
+  });
+});
