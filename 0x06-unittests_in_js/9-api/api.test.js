45079390ff760a55d5f562e84e5472322f7fb0b3
AhmedEhab2022 <am8122004@gmail.com>
2024-06-20 22:36:51 +0300

    9. Regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,41 @@
+const request = require('request');
+const chai = require('chai');
+const { expect } = chai;
+// import regex module
+
+describe('Index page', () => {
+  const baseUrl = 'http://localhost:7865';
+
+  it('Correct status code?', (done) => {
+    request.get(baseUrl, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('Correct result?', (done) => {
+    request.get(baseUrl, (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('Cart page', () => {
+  const pattern = /^Payment methods for cart \d+$/;
+  const baseUrl = 'http://localhost:7865/cart/12';
+
+  it('Correct status code?', (done) => {
+    request.get(baseUrl, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('Correct result?', (done) => {
+    request.get(baseUrl, (error, response, body) => {
+      expect(body).to.match(pattern);
+      done();
+    });
+  });
+});
