5041ae85e2161761e33fcb9e5bae587993400818
AhmedEhab2022 <am8122004@gmail.com>
2024-06-21 20:00:25 +0300

    use expect

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,52 +1,52 @@
 const calculateNumber = require('./1-calcul.js');
-const assert = require('assert');
+const { expect } = require('chai')
 
 describe('calculateNumber', () => {
   it('Sum', () => {
-    assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
-    assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
-    assert.strictEqual(calculateNumber('SUM', 1.2, 3.7 ), 5);
-    assert.strictEqual(calculateNumber('SUM', 1.5, 3.7 ), 6);
-    assert.strictEqual(calculateNumber('SUM', 1.4, 3.7 ), 5);
-    assert.strictEqual(calculateNumber('SUM', 0.0, 3.7 ), 4);
-    assert.strictEqual(calculateNumber('SUM', 1.4, 3.2 ), 4);
-    assert.strictEqual(calculateNumber('SUM', 1.4, 3.5 ), 5);
-    assert.strictEqual(calculateNumber('SUM', -1.4, -3.5), -4);
-    assert.strictEqual(calculateNumber('SUM', -1.4, 3.5), 3);
-    assert.strictEqual(calculateNumber('SUM', 1.4, -3.5), -2);
-    assert.strictEqual(calculateNumber('SUM', 0.1, 0.0), 0);
+    expect(calculateNumber('SUM', 1, 3), 4).to.equal(4);
+    expect(calculateNumber('SUM', 1, 3.7), 5).to.equal(5);
+    expect(calculateNumber('SUM', 1.2, 3.7 )).to.equal(5);
+    expect(calculateNumber('SUM', 1.5, 3.7 )).to.equal(6);
+    expect(calculateNumber('SUM', 1.4, 3.7 )).to.equal(5);
+    expect(calculateNumber('SUM', 0.0, 3.7 )).to.equal(4);
+    expect(calculateNumber('SUM', 1.4, 3.2 )).to.equal(4);
+    expect(calculateNumber('SUM', 1.4, 3.5 )).to.equal(5);
+    expect(calculateNumber('SUM', -1.4, -3.5)).to.equal(-4);
+    expect(calculateNumber('SUM', -1.4, 3.5)).to.equal(3);
+    expect(calculateNumber('SUM', 1.4, -3.5)).to.equal(-2);
+    expect(calculateNumber('SUM', 0.1, 0.0)).to.equal(0);
   });
 
   it('Subtraction', () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', 1, 3), -2);
-    assert.strictEqual(calculateNumber('SUBTRACT', 1, 3.7), -3);
-    assert.strictEqual(calculateNumber('SUBTRACT', 1.2, 3.7), -3);
-    assert.strictEqual(calculateNumber('SUBTRACT', 1.5, 3.7), -2);
-    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 3.7), -3);
-    assert.strictEqual(calculateNumber('SUBTRACT', 0.0, 3.7), -4);
-    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 3.2), -2);
-    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 3.5), -3);
-    assert.strictEqual(calculateNumber('SUBTRACT', -1.4, -3.5), 2);
-    assert.strictEqual(calculateNumber('SUBTRACT', -1.4, 3.5), -5);
-    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, -3.5), 4);
+    expect(calculateNumber('SUBTRACT', 1, 3)).to.equal(-2);
+    expect(calculateNumber('SUBTRACT', 1, 3.7)).to.equal(-3);
+    expect(calculateNumber('SUBTRACT', 1.2, 3.7)).to.equal(-3);
+    expect(calculateNumber('SUBTRACT', 1.5, 3.7)).to.equal(-2);
+    expect(calculateNumber('SUBTRACT', 1.4, 3.7)).to.equal(-3);
+    expect(calculateNumber('SUBTRACT', 0.0, 3.7)).to.equal(-4);
+    expect(calculateNumber('SUBTRACT', 1.4, 3.2)).to.equal(-2);
+    expect(calculateNumber('SUBTRACT', 1.4, 3.5)).to.equal(-3);
+    expect(calculateNumber('SUBTRACT', -1.4, -3.5)).to.equal(2);
+    expect(calculateNumber('SUBTRACT', -1.4, 3.5)).to.equal(-5);
+    expect(calculateNumber('SUBTRACT', 1.4, -3.5)).to.equal(4);
   });
 
   it('Division', () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 1, 3), 1 / 3);
-    assert.strictEqual(calculateNumber('DIVIDE', 1, 3.7), 1 / 4);
-    assert.strictEqual(calculateNumber('DIVIDE', 1.2, 3.7), 1 / 4);
-    assert.strictEqual(calculateNumber('DIVIDE', 1.5, 3.7), 2 / 4);
-    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 3.7), 1 / 4);
-    assert.strictEqual(calculateNumber('DIVIDE', 0.0, 3.7), 0);
-    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 3.2), 1 / 3);
-    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 3.5), 1 / 4);
-    assert.strictEqual(calculateNumber('DIVIDE', -1.4, -3.5), -1 / -3);
-    assert.strictEqual(calculateNumber('DIVIDE', -1.4, 3.5), -1 / 4);
-    assert.strictEqual(calculateNumber('DIVIDE', 1.4, -3.5), 1 / -3);
-    assert.strictEqual(calculateNumber('DIVIDE', 0.1, 0.0), 'Error');
+    expect(calculateNumber('DIVIDE', 1, 3)).to.equal(1 / 3);
+    expect(calculateNumber('DIVIDE', 1, 3.7)).to.equal(1 / 4);
+    expect(calculateNumber('DIVIDE', 1.2, 3.7)).to.equal(1 / 4);
+    expect(calculateNumber('DIVIDE', 1.5, 3.7)).to.equal(2 / 4);
+    expect(calculateNumber('DIVIDE', 1.4, 3.7)).to.equal(1 / 4);
+    expect(calculateNumber('DIVIDE', 0.0, 3.7)).to.equal(0);
+    expect(calculateNumber('DIVIDE', 1.4, 3.2)).to.equal(1 / 3);
+    expect(calculateNumber('DIVIDE', 1.4, 3.5)).to.equal(1 / 4);
+    expect(calculateNumber('DIVIDE', -1.4, -3.5)).to.equal(1 / 3);
+    expect(calculateNumber('DIVIDE', -1.4, 3.5)).to.equal(-1 / 4);
+    expect(calculateNumber('DIVIDE', 1.4, -3.5)).to.equal(1 / -3);
+    expect(calculateNumber('DIVIDE', 0.1, 0.0)).to.equal('Error');
   });
 
   it('Invalid type', () => {
-    assert.strictEqual(calculateNumber('INVALID', 1, 3), 'Error');
+    expect(calculateNumber('INVALID', 1, 3)).to.equal('Error');
   });
 });
